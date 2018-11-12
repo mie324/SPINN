43d0c8d81cabbb1ea06234343dac84ee2a2ad29e
tinahe75 <heyan_75@163.com>
2018-11-11 20:02:54 -0800

    Subtask 1 initial model

---
+++ subtask1.py
@@ -0,0 +1,176 @@
+import torch
+import torch.optim as optim
+import torchtext
+from torchtext import data
+import spacy
+import argparse
+import os
+import numpy as np
+from subtask1_model import *
+
+torch.manual_seed(77)
+bs,lr,MaxEpochs,model_type,num_filters,rnn_hidden_dim,embed_dim = None,None,None,None,None,None,None
+
+
+bs = 64
+def build_iter():
+    global bs
+    TEXT = data.Field(sequential=True, include_lengths=True, tokenize='spacy')
+    DETECTION_LABEL = data.Field(sequential=False, use_vocab=False)
+    fields = [('text', TEXT), ('detection', DETECTION_LABEL)]
+    train, val, test = data.TabularDataset.splits(path = '',train='train1.tsv', validation='validation1.tsv',
+                                                  test='test1.tsv', format='tsv', fields=fields, skip_header=True)
+    train_iter, val_iter, test_iter = data.Iterator.splits((train, val, test), sort_key=lambda x: len(x.text),
+                                                           batch_sizes=(bs, bs, bs),
+                                                           sort_within_batch=True,
+                                                           repeat=False)
+
+    TEXT.build_vocab(train,val,test)
+    TEXT.vocab.load_vectors(torchtext.vocab.GloVe(name='6B', dim=200))
+    return train_iter, val_iter, test_iter, TEXT.vocab
+
+
+
+'''
+LOAD MODEL
+'''
+def load_model(learning_rate,vocab):
+    global model_type,rnn_hidden_dim,embed_dim
+    if model_type == 'baseline':
+        model = Baseline(embed_dim,vocab) # will use TEXT.vocab
+    elif model_type == 'rnn':
+        model = RNN(embed_dim,vocab,rnn_hidden_dim)
+    elif model_type == 'cnn':
+        model = CNN(embed_dim, vocab, num_filters, np.array([2,4]))
+    loss_fxn = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    return model,loss_fxn,optimizer
+
+
+
+'''
+EVALUATE RESULTS WITH VALIDATION SET
+'''
+def evaluate_model(model,loss_fxn,val_iter):
+    global bs
+    total_val_corr = 0
+    accum_loss = 0
+    vali_samples = len(val_iter.dataset)
+    for i,data in enumerate(val_iter):
+        (x, x_lengths), y = data.text, data.detection
+        predictions = model.forward(x,x_lengths)
+        loss = loss_fxn(input=predictions.squeeze(), target=y.float())
+        accum_loss += loss.item()
+
+        # compare batch predictions to labels
+        pred_res = predictions.data.squeeze().numpy()
+        actual_res = y.int().numpy()
+        for j in range(len(y)):
+            if pred_res[j] <= 0.5 and actual_res[j] == 0:
+                total_val_corr += 1
+            elif pred_res[j] > 0.5 and actual_res[j] == 1:
+                total_val_corr += 1
+
+    eval_accuracy = float(total_val_corr)/vali_samples
+    return accum_loss/(i+1),eval_accuracy  # return this as the evaluation accuracy
+
+
+
+'''
+TRAINING AND EVALUATION
+'''
+def main(args):
+    dict_args = vars(args)
+    global bs, lr, MaxEpochs, model_type,eval_every,num_filters,rnn_hidden_dim,embed_dim
+    bs = dict_args['batch_size']
+    lr = dict_args['lr']
+    MaxEpochs = dict_args['epochs']
+    model_type = dict_args['model']
+    num_filters = dict_args['num_filt']
+    rnn_hidden_dim = dict_args['rnn_hidden_dim']
+    embed_dim = dict_args['emb_dim']
+    eval_every = int(1640 / bs)
+
+    train_iter, val_iter, test_iter,vocab = build_iter()
+    model,loss_fxn,optimizer = load_model(lr,vocab)
+    batch_done = 0
+    #max_vali_accuracy = 0
+    #lowest_vali_loss = 0
+
+    for epoch in range(MaxEpochs):
+        accum_loss = float(0)
+        total_correct = 0
+
+        for i,data in enumerate(train_iter):
+            (x, x_lengths), y = data.text, data.detection
+
+            optimizer.zero_grad()
+            predictions = model.forward(x,x_lengths)
+
+            loss = loss_fxn(input=predictions.squeeze(),target=y.float())
+            accum_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
+
+            # comopare batch predictions to labels
+            pred_res = predictions.data.squeeze().numpy()
+            actual_res = y.int().numpy()
+            for j in range(len(y)):
+                if pred_res[j] <= 0.5 and actual_res [j] ==0:
+                    total_correct +=1
+                elif pred_res[j] > 0.5 and actual_res [j] ==1:
+                    total_correct +=1
+
+            if batch_done == 0:
+                print('\n ---------------- T R A I N I N G   I N   P R O G R E S S ----------------\n')
+            #if (batch_done + 1) % eval_every == 0:
+                #vali_loss,vali_accuracy = evaluate_model(model, loss_fxn,val_iter)
+                #vali_accuracy_log[epoch] = vali_accuracy
+                #if vali_accuracy > max_vali_accuracy:
+                    #max_vali_accuracy = vali_accuracy
+                    #lowest_vali_loss = vali_loss
+                #print('\n Epoch {}, after {} total batches, accum loss is {}, validation accuracy is {} \n'.format(
+                    #epoch + 1, batch_done + 1, accum_loss / 100, vali_accuracy))
+                #batch_number_log[epoch] = batch_done + 1
+            batch_done += 1
+
+        vali_loss, vali_accuracy = evaluate_model(model, loss_fxn, val_iter)
+        train_loss = accum_loss/(i+1)
+        train_accuracy = total_correct / len(train_iter.dataset)
+        #train_accuracy_log[epoch] = train_accuracy
+        print('\n Epoch {}, after {} total batches, accum loss is {}, validation accuracy is {} \n'.format(
+            epoch + 1, batch_done, train_loss, vali_accuracy))
+        print('------------- Training accuracy at end of epoch is {} -------------\n'.format(train_accuracy))
+
+
+    #torch.save(model,'model_{}.pt'.format(model_type))
+    test_loss,test_accuracy = evaluate_model(model,loss_fxn,test_iter)
+    print("================ RESULTS FOR MODEL: {} ================\n".format(model_type))
+    # Use the results from the last epoch as loss and accuracy for training set:
+    print("TRAIN SET: loss = {}, accuracy = {}".format(train_loss,train_accuracy))
+    # Use the results from the last epoch as loss and accuracy for validation set:
+    print("VALIDATION SET: loss = {}, accuracy = {}".format(vali_loss,vali_accuracy))
+    print("TEST SET: loss = {}, accuracy = {}".format(test_loss, test_accuracy))
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch-size', type=int, default=64)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--epochs', type=int, default=25)
+    parser.add_argument('--model', type=str, default='cnn',
+                        help="Model type: baseline,rnn,cnn (Default: baseline)")
+    parser.add_argument('--emb-dim', type=int, default=200)
+    parser.add_argument('--rnn-hidden-dim', type=int, default=100)
+    parser.add_argument('--num-filt', type=int, default=50)
+
+    args = parser.parse_args()
+
+    main(args)
+
+
---
+++ subtask1_model.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+vocab_size = 6247
+torch.manual_seed(77)
+
+
+# 4.2 BASELINE MODEL
+
+# class Baseline(nn.Module):
+#     def __init__(self, embedding_dim, vocab):
+#         super(Baseline, self).__init__()
+#         self.embedding_layer = nn.Embedding(vocab_size,embedding_dim) ## initialize
+#         self.embedding_layer = nn.Embedding.from_pretrained(vocab.vectors)
+#         self.fc1 = nn.Linear(embedding_dim,1)
+#
+#     def forward(self, x, lengths):
+#         x = self.embedding_layer(x)
+#         x = torch.mean(x,0)
+#         x = self.fc1(x)
+#         activation = nn.Sigmoid()
+#         x = activation(x)
+#         return x
+
+class Baseline(nn.Module):
+    def __init__(self, embedding_dim, vocab):
+        super(Baseline, self).__init__()
+
+        ######
+        # 4.1 YOUR CODE HERE
+        self.embed = nn.Embedding.from_pretrained(vocab.vectors)
+        self.fc1 = nn.Linear(embedding_dim, 1, bias=True)
+        ######
+
+    def forward(self, x, lengths=None):
+
+        ######
+
+        x = self.embed(x)
+        x = torch.sum(x, dim=0)
+        lengths = 1/lengths.float()
+        x = torch.transpose(x.float(), 0, 1) * lengths
+        x = torch.transpose(x, 0, 1)
+        x = self.fc1(x)
+        x = torch.sigmoid(x.squeeze())
+        return x
+        # 4.1 YOUR CODE HERE
+
+        ######
+
+
+# 4.3 RNN MODEL
+
+class RNN(nn.Module):
+    def __init__(self, embedding_dim, vocab, hidden_dim):
+        super(RNN, self).__init__()
+        self.embedding_layer = nn.Embedding.from_pretrained(vocab.vectors)
+        self.gru = nn.GRU(embedding_dim,hidden_dim)
+        self.fc1 = nn.Linear(hidden_dim,1)
+
+    def forward(self, x,lengths):  # pass in x and x_length
+        x = self.embedding_layer(x)
+        x = nn.utils.rnn.pack_padded_sequence(x,lengths)
+        x = self.gru(x)[1]  # just take the hidden state of the output
+        x = x.squeeze()
+        x = self.fc1(x)
+        activation = nn.Sigmoid()
+        x = activation(x)
+        return x
+
+
+
+
+ # 4.4 CNN MODEL
+
+class CNN(nn.Module):
+    def __init__(self, embedding_dim, vocab, n_filters, filter_sizes):
+        super(CNN, self).__init__()
+        self.embedding_layer = nn.Embedding.from_pretrained(vocab.vectors)
+        self.conv1 = nn.Conv2d(1,out_channels=n_filters,kernel_size=(filter_sizes[0],embedding_dim))
+        self.conv2 = nn.Conv2d(1,n_filters,(filter_sizes[1],embedding_dim))
+        self.fc1 = nn.Linear(n_filters*2,1)
+
+    def forward(self, x, lengths):
+        x = torch.transpose(x,0,1)   # make it into dim = (bs, sentence length)
+        x = self.embedding_layer(x)   # make it into dim = (bs,sentence length, embedding dim)
+        shape = x.shape
+        x = x.view(shape[0],1,shape[1],shape[2])
+        x1 = self.conv1(x)   # do convolution using kernel size 2, get dim = (bs,filter num, feature map size,1)
+        x2 = self.conv2(x)    # do convolution using kernel size 4
+        relu = nn.ReLU()
+        x1 = relu(x1)
+        x2 = relu(x2)
+        x1 = torch.max(x1,2)[0]    # max pool of results using kernel size = 2,  get dim = (bs, filter num, 1)
+        x2 = torch.max(x2,2)[0]     # max pool of results using kernel size = 4
+        x = torch.cat((x1,x2),1)
+        x = x.squeeze()   # get dim = (bs,100)
+        x = self.fc1(x)
+        sigmoid = nn.Sigmoid()
+        x = sigmoid(x)
+        return x
