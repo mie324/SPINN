4d63b37258a1038224d5371d39d98de25fca63f8
tinahe75 <heyan_75@163.com>
2018-11-25 09:15:13 -0800

    Calculated sensegram scores for sentences, split score data

---
+++ split_data_sensegram.py
@@ -0,0 +1,28 @@
+import numpy as np
+import spacy
+import pandas as pd
+# CHANGE SPLIT RATIO HERE
+train_num = 820
+val_num = 174
+test_num = 261
+
+task = 'location'
+
+pun_df = pd.read_table('sensegram_scores.tsv')
+pun_df_shuffled = pun_df.sample(frac=1,random_state=77)
+
+# split into 3 sets
+train_df = pun_df_shuffled.iloc[0:train_num,:]
+train_df = train_df.sample(frac=1,random_state=77)
+train_df_1 = train_df[['score',task]]
+train_df_1.to_csv('train1{}_sensegram.tsv'.format(task), sep='\t',index=False)  # must set index to False!!! otherwise format incorrect
+
+val_df = pun_df_shuffled.iloc[train_num:(train_num+val_num),:]
+val_df = val_df.sample(frac=1,random_state=77)
+val_df_1 = val_df[['score',task]]
+val_df_1.to_csv('validation1{}_sensegram.tsv'.format(task), sep='\t',index=False)
+
+test_df = pun_df_shuffled.iloc[(train_num+val_num)::,:]
+test_df = test_df.sample(frac=1,random_state=77)
+test_df_1 = test_df[['score',task]]
+test_df_1.to_csv('test1{}_sensegram.tsv'.format(task), sep='\t',index=False)
