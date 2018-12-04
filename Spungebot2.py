d6e5534ba2b2bb10e65e3e8db8cb3c69a7527438
tinahe75 <heyan_75@163.com>
2018-12-03 17:29:19 -0800

    added latest user interface and scripts for getting wsd scores for dataset

--- Spungebot2.py
+++ Spungebot2.py
@@ -33,6 +33,19 @@ def evaluate(model, data,length):
     return prediction
 
 
+def evaluate_task1(model, data,length):
+    model.eval()
+    data = data.long()
+
+    prediction = model(data, torch.tensor([length]))
+
+    if prediction > 0.5:
+        return prediction, 'pun'
+    else:
+        return 1-prediction, 'not pun'
+
+
+
 TEXT = torchtext.data.Field(sequential=True, tokenize='spacy', include_lengths=True)
 LABEL = torchtext.data.Field(sequential=False, use_vocab=False)
 train, val, test = torchtext.data.TabularDataset.splits(
@@ -108,7 +121,7 @@ def evaluate_sg_detection(model,sentence_df):
 while True:
     ans = ''
     while ans=='':
-        context = input("Enter a sentence: \n ")
+        context = input("\nEnter a sentence: \n ")
         ans = context.replace(',', '')
         ans = ans.replace('.', '')
         if len(ans.split(sep=" "))<4:
@@ -126,6 +139,7 @@ while True:
 
     wsd_model = WSD(sv, wv, window=context_window_size, lang='en',ignore_case=True)
     word_list = context.split(' ')
+
     sentence_df = sensegram(context,word_list,context_window_size)
 
 
@@ -133,23 +147,55 @@ while True:
     j = 0
     prob = 0.0
 
+    # SUBTASK 1 with only RNN
+    model4 = torch.load('model_rnn.pt')
+    predictions4, label4 = evaluate_task1(model4, torch.from_numpy(inputs), length)
+    predictions4 = predictions4.detach().numpy()
+    print('RNN model thinks the sentence is {}, with {}% confidence'.format(label4, str(100 * round(float(predictions4), 3))))
 
+
+    # SUBTASK 1 with SENSEGRAM
     model3 = torch.load('model_rnn_sgWSDdetection.pt')
     prob, label3 = evaluate_sg_detection(model3, sentence_df)
     if label3 == 1:
         x = 'pun'
     else:
         x = 'not pun'
-    print('WSD model thinks the sentence is {}, with {}% confidence'.format(x, round(prob, 3) * 100))
 
-    model1 = torch.load("model_{}location.pt".format(list[0]))
-    label1 = evaluate(model1, torch.from_numpy(inputs),torch.tensor(length))
-    print('{} model thinks the punning word is "{}".'.format(list[0].capitalize(),sentence[int(label1)]))
+    WSD_str = str(round(float(prob), 3) * 100)
+    if len(WSD_str) > 4:
+        WSD_str = WSD_str[:4]
+    print('WSD+RNN model thinks the sentence is '+ x +' with '+WSD_str+'% confidence')
 
-    model2 = torch.load("model_{}location.pt".format(list[1]))
-    label2 = evaluate_sensegram(model2, sentence_df)
-    print('{} model thinks the punning word is "{}".'.format(list[1].capitalize(),word_list[int(label2)]))
+    request = ''
+
+    if label4 == 'not pun' and x == 'not pun':
+        request = input('\nDo you want to locate a possible punning word in this sentence? (yes/no)')
+    if label4 == 'pun' or x == 'pun' or request =='yes':
+
+        #SUBTASK 2 with rnn
+        model1 = torch.load("model_{}location.pt".format(list[0]))
+        label1 = evaluate(model1, torch.from_numpy(inputs),torch.tensor(length))
+        pun_word_1 = sentence[int(label1)]
+        print('{} model thinks the punning word is "{}".'.format(list[0].capitalize(),pun_word_1))
+
+
+        #SUBTASK 2 with sensegram_rnn
+        model2 = torch.load("model_{}location.pt".format(list[1]))
+        label2 = evaluate_sensegram(model2, sentence_df)
+        pun_word_2 = word_list[int(label2)]
+        print('{} model thinks the punning word is "{}".'.format(list[1].capitalize(),pun_word_2))
 
 
 
-    print('\nTo look up the definition, you can access: https://www.google.ca/search?q={}%20definition\n'.format(sentence[int(label1)]))
+        print('\nTo look up the definition of {}, you can go to: https://www.google.ca/search?q={}%20definition'.format(pun_word_1,pun_word_1))
+        if str(pun_word_2) != str(pun_word_1):
+            print('To look up the definition of {}, you can go to: https://www.google.ca/search?q={}%20definition'.format(pun_word_2, pun_word_2))
+
+
+'''
+###  PRESENTATION DEMO  ###
+We dressed up as almonds for Halloween . People say we ' re nuts !
+
+
+'''
---
+++ get_df_location.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import torch
+import sensegram
+from wsd import WSD
+from gensim.models import KeyedVectors
+
+orig_data_df = pd.read_csv('sentences_balanced_revised.csv')   # save original tsv file to pd dataframe
+pun_df = orig_data_df.iloc[0:1255,:]  # contains 1255 puns
+
+#location_array = pun_df['location'].values
+#np.save('sensegram_location_arr.npy',location_array)
+
+sensegram_df = pd.DataFrame()
+#stacked_score_tensor= torch.ones(2)
+
+# make sure the sentences csv file is in the same directory!
+sense_vectors_fpath = "model/wiki.txt.clusters.minsize5-1000-sum-score-20.sense_vectors"
+word_vectors_fpath = "model/wiki.txt.word_vectors"
+
+# Load models (takes long time)
+sv = sensegram.SenseGram.load_word2vec_format(sense_vectors_fpath, binary=False)
+wv = KeyedVectors.load_word2vec_format(word_vectors_fpath, binary=False, unicode_errors="ignore")
+context_words_max = 20 # change this paramters to 1, 2, 5, 10, 15, 20 : it may improve the results
+context_window_size = 10 # this parameters can be also changed during experiments
+
+
+
+
+def filter_scores(array):
+    if array.size == 1:  # probably punctuation
+        return np.negative(np.ones(10))
+    if array.size > 10:   #too many scores, so let's just keep ten
+        return array[:10]
+    while array.size <10:  # too few scores, pad with -1
+        array = np.append(array,np.array([-1]))
+    return array
+
+
+ignore_case = True
+lang = "en"  # to filter out stopwords
+for i in range(1255):
+    sentence = pun_df['text'][i]
+    location = pun_df['location'][i]
+    word_list = sentence.split(' ')
+    sentence_score_array = np.array([])
+    for word in word_list:
+        wsd_model = WSD(sv, wv, window=context_window_size, lang=lang,ignore_case=ignore_case)
+        word_scores_list = wsd_model.disambiguate(sentence, word)[1]
+            #print(word_scores_list)
+        word_scores_list = sorted(word_scores_list,reverse=True)
+        word_scores_arr = np.asarray(word_scores_list)
+        word_scores_arr = filter_scores(word_scores_arr)
+        sentence_score_array = np.concatenate((sentence_score_array,word_scores_arr),axis=0)
+    sentence_score_array = np.reshape(sentence_score_array,(len(word_list),10))
+    sensegram_df = sensegram_df.append({'score':sentence_score_array,'location':location},ignore_index=True)
+
+    '''
+    sentence_tensor = torch.from_numpy(sentence_score_array)
+    sentence_tensor = sentence_tensor.view(1,len(word_list),10)
+    if i==0:
+        stacked_score_tensor = sentence_tensor
+    else:
+        stacked_score_tensor = torch.cat((stacked_score_tensor,sentence_tensor),0)
+    '''
+
+    print('{}\n'.format(i))
+
+sensegram_df.to_csv('sensegram_scores.tsv', sep='\t',index=False)
+sensegram_df.to_pickle('sensegram_scores.pkl')
+
+#score_array = stacked_score_tensor.numpy()
+#np.save('sensegram_score_arr.npy',score_array)
+
+
